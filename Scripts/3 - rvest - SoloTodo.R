b83df7609deeae5ea633fb174d33bfba0a16de5c
Víctor Caquilpan <victor.caquilpan@gmail.com>
2021-10-08 11:02:18 -0300

    Script WebScraping

---
+++ Scripts/3 - rvest - SoloTodo.R
@@ -0,0 +1,68 @@
+# Instalación de librerías
+# install.packages("dplyr")
+# install.packages("stringr")
+# install.packages("rvest")
+
+# Carga de librerías ----
+library(dplyr)   # Manejo de datos
+library(stringr) # Manejo de texto
+library(rvest)   # Raspado Web 
+
+# Pagina para extracción de información - PCFactory
+url <- "https://www.solotodo.cl/notebooks?score_games_start=450&ordering=offer_price_usd&"
+
+# Leemos la página 
+lectura_pagina <- read_html(url)
+
+# Vemos que podemos extraer el nombre de los notebooks disponibles
+lectura_pagina %>% 
+  html_elements(css = "#category-browse-results-card h3") %>% html_text2()
+
+# Hagamos algo más complejo y útil. Extraigamos el nombre y el precio
+lectura_pagina %>% 
+  html_elements(css = "#category-browse-results-card .flex-grow a , #category-browse-results-card h3") %>% 
+  html_text2()
+
+# Ahora que sabemos cómo extraer la inforamación, tenemos que ver cómo dejarla en un dataframe
+datos_solotodo <- lectura_pagina %>% 
+                  html_elements(css = "#category-browse-results-card .flex-grow a , #category-browse-results-card h3") %>% 
+                  html_text2()
+
+# Podemos idear alguna forma para extraer los valores impares: nombres, y pares: precios
+which(1:length(datos_solotodo) %% 2 == 1)
+
+# Creamos tabla
+Notebook <- datos_solotodo[which(1:length(datos_solotodo) %% 2 == 1)]
+Precio <- datos_solotodo[which(1:length(datos_solotodo) %% 2 == 0)]
+Tabla <- data.frame(Notebook,Precio)
+
+# Ya logramos extraer la información de una página ¿Podemos extraer la información de las demás páginas?
+
+# Creamos un dataframe vacio
+Tabla_resultado <- data.frame(Notebook = as.character(),
+                              Precio = as.character())
+
+for (pagina in 1:22) {
+  
+  # Seteamos la página que va a ir variando en el ciclo
+  url <- str_c("https://www.solotodo.cl/notebooks?score_games_start=450&ordering=offer_price_usd&page=",pagina)
+  
+  # Colocamos una mensaje en pantalla
+  print(str_c("Leyendo página: ",pagina))
+  
+  # Extraemos la información
+  datos_solotodo <- read_html(url) %>% # vamos a ocupar read_html para que sea más rápido este proceso 
+    html_elements(css = "#category-browse-results-card .flex-grow a , #category-browse-results-card h3") %>% 
+    html_text2()
+  
+  # Creamos tabla
+  Notebook <- datos_solotodo[which(1:length(datos_solotodo) %% 2 == 1)]
+  Precio <- datos_solotodo[which(1:length(datos_solotodo) %% 2 == 0)]
+  Tabla <- data.frame(Notebook,Precio)
+  
+  # Apendizamos nuestros datos a la tabla final
+  Tabla_resultado <- rbind(Tabla_resultado,Tabla)
+  
+}
+
+
---
+++ Scripts/6 - RSelenium - INaturalist.R
@@ -0,0 +1,115 @@
+# Instalación de librerías
+# install.packages("dplyr")
+# install.packages("stringr")
+# install.packages("rvest")
+# install.packages("RSelenium")
+
+# Carga de librerías ----
+library(dplyr)     # Manejo de datos
+library(stringr)   # Manejo de texto
+library(rvest)     # Raspado Web
+library(RSelenium) # Raspado Web
+
+# Vamos a ingresar a la página de Inaturalist
+url <- "https://www.inaturalist.org/observations"
+
+# Iniciamos servidor
+driver <-  rsDriver(port = 4571L, 
+                    browser = "chrome",
+                    chromever = "94.0.4606.41") 
+
+# Separamos el cliente/servidor
+navegador <- driver$client
+servidor <- driver$server
+
+# Ingresamos a la pagina de interes
+navegador$navigate(url = url)
+
+# Supongamos que queremos buscar tres especies de aves en Chile. Martin pescador de collar, Zorzal Patagónico y Pato jergon
+
+# Primero ingresamos la información de búsqueda
+ingreso_especie <- navegador$findElement(using = "name",value = "taxon_name")
+ingreso_especie$highlightElement()
+ingreso_especie$sendKeysToElement(list("Martin pescador de Collar"))
+chequeo_especie <- navegador$findElement(using = "css",value = "#ui-id-2")
+chequeo_especie$clickElement()
+
+# Ahora ingresamos el país
+ingreso_lugar <- navegador$findElement(using = "name",value = "primary_q2")
+ingreso_lugar$sendKeysToElement(list("Chile",key = "enter"))
+
+# Esperemos unos segundos 
+Sys.sleep(5)
+
+# Vamos a la primera imagen
+imagen1 <- navegador$findElement(using = "css",value = "#result-grid > div > div:nth-child(1) > div > a")
+
+# Obtenemos el enlace de la imagen
+imagen1$getElementAttribute(attrName = "style")
+
+# Nos quedamos solo con la parte del enlace
+enlace_martin <- imagen1$getElementAttribute(attrName = "style")[[1]]
+enlace_martin <- str_extract(enlace_martin,pattern = 'https:.*')
+
+# Descargamos imagen con download.file
+download.file(url = enlace_martin,destfile = "martin1.jpg",mode = "wb")
+
+# Podemos hacer lo mismo para las demás imagenes
+inaturalist <- navegador$getPageSource()[[1]] %>% read_html()
+enlaces_martines <- inaturalist %>% html_elements(css = ".has-photo") %>% html_attr(name = "style")
+enlaces_martines <- str_extract(enlaces_martines,pattern = 'https:.*')
+
+# Creamos un ciclo para descargar
+for (archivo in 1:10) { # descargamos las primeras 10 imagenes
+  
+  download.file(enlaces_martines[archivo],destfile = str_c(archivo,"_martin.jpg"),mode = "wb")
+  
+}
+
+# Podemos repetir este mismo proceso para las demás especies. El siguiente segmento de código realiza todo el proceso en conjunto
+
+
+especies <- c("Martin pescador de Collar","Zorzal Patagónico","Pato jergon")
+
+# Creamos un ciclo for
+for (especie in 1:length(especies)) {
+
+  # Ingresamos a la pagina de interes
+  navegador$navigate(url = url)  
+  
+  # Esperamos unos segundos a que cargue página
+  Sys.sleep(3)
+
+  # Primero ingresamos la información de búsqueda
+  ingreso_especie <- navegador$findElement(using = "name",value = "taxon_name")
+  ingreso_especie$sendKeysToElement(list(especies[especie]))
+  # Esperamos un par de segundos
+  Sys.sleep(2)
+  chequeo_especie <- navegador$findElement(using = "css",value = "#ui-id-2")
+  chequeo_especie$clickElement()
+  
+  # Ahora ingresamos el país
+  ingreso_lugar <- navegador$findElement(using = "name",value = "primary_q2")
+  ingreso_lugar$sendKeysToElement(list("Chile",key = "enter"))
+  
+  # Esperemos unos segundos 
+  Sys.sleep(5)
+  
+  # Creamos los objetos resultantes
+  assign(str_c("enlaces",especies[especie]),
+         value = navegador$getPageSource()[[1]] %>% 
+           read_html() %>% 
+           html_elements(css = ".has-photo") %>% 
+           html_attr(name = "style") %>%
+           str_extract(pattern = 'https:.*')) 
+  
+  # Generamos una pausa
+  Sys.sleep(2)
+  
+}
+
+# Podemos chequear los objetos creados con los respectivos enlaces
+
+# Cerramos navegador y servidor
+navegador$close()
+system(paste0("Taskkill /F /T" ," /PID ", driver$server$process$get_pid()))
